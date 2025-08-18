3d80fe92e6abdc3a434d4ba4e023ef1c5e9d7824
Shubham Tripathi <shubham.tripathi4@outlook.com>
2025-08-18 23:33:53 +0530

    Refactor src/reducer/authedUser.js

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -4,8 +4,6 @@ const authedUser = (state = null, action) => {
   switch (action.type) {
     case SET_AUTHED_USER:
       return action.id;
-    // case RESET_AUTHED_USER:
-    //   return null;
 
     default:
       return state;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import { loadingBarReducer } from "react-redux-loading-bar";
-import authedUser from "./autherUser";
+import authedUser from "./authedUser";
 import users from "./users";
 import questions from "./questions";
 
